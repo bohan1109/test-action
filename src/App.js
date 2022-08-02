2619c6aed89a234f36d6afd3f07d0829ca375dd5
bohan1109 <4A790162@stust.edu.tw>
2022-08-02 13:45:34 +0800

    your commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as React from "react";
+import axios from './axios.config'
 
 function App() {
+  const click = () => {
+    const query = `
+    query Account($username:String){
+      account(username:$username){
+        name
+        age
+        department
+        sex
+        salary(city:"台北")
+      }
+    }
+    `;
+    const variables = { username: "han" };
+    axios
+    .post('graphql',
+    {
+      query:query,
+      variables:variables
+    },{
+      headers:{
+        "content-type": "application/json",
+        "Accept": "application/json"
+      }
+    }
+    ).then(response =>console.log(response))
+    .catch(err =>console.log(err))
+    // fetch("http://localhost:5000/graphql", {
+    //   mode:'no-cors',
+    //   method: "POST",
+    //   headers: {
+    //     "content-type": "application/json",
+    //     "Accept": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     query: query,
+    //     variables: variables,
+    //   })
+      
+    // }).then((res) => res.json())
+    //   .then(res => console.log(res))
+    //   .catch((err) => console.log(err))
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button type="button" onClick={click}>test</button>
     </div>
   );
 }
